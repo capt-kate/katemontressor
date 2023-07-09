e18cad3d84bbcaf983651a6567a38ba766c2deeb
Kate Montressor <katemontressor@gmail.com>
2023-07-09 16:45:20 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html>
+<head>
+	<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-0RJSHKZH0T"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-0RJSHKZH0T');
+</script>
+    <meta charset="UTF-8">
+    <title>Kate Montressor</title>
+    <meta name="keywords" content="Kate Montressor madcap flare advanced developer technical writer UI design project manager program API user guide javascript html5 css3 developer jquery excellent superior experienced happy curious quick learner fast up-to-speed expeditious prompt intelligent perceptive smart skillful discerning dexterous energetic alert" />
+ 
+    <!-- for-mobile-apps -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ 
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);function hideURLbar(){ window.scrollTo(0,1); } </script>
+    
+    <!-- CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/kate-style.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/responsive.css" rel="stylesheet" type="text/css" media="all" />
+        
+    <!-- JS -->
+    <script src="js/jquery.min.js"> </script>
+    <script src="js/bootstrap.js"></script>
+
+    
+</head>
+
+<body>s
+
+<?php include("nav.php"); ?>
+
+<!----------- PHOTO ----------->
+<?php include("photo.php"); ?>
+
+<br />
+
+<!----------- ABOUT ----------->
+<?php include("about.php"); ?>
+             
+<br />
+
+<!----------- DETAILS ----------->
+<?php include("details.php"); ?>
+
+<br />
+
+<!----------- SKILLS ----------->
+<?php include("skills.php"); ?>
+
+<br />
+           
+<!----------- PORTFOLIO ----------->
+<?php include("portfolio.php"); ?>
+
+<br />
+           
+<!----------- CONTACT ----------->
+<?php include("contact.php"); ?>
+       
+
+<!----------- FOOTER ----------->
+<?php include("FOOTER.php"); ?>
+
+
+
+
+</body>
+</html>
